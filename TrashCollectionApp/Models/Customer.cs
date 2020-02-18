d44c66f28317fb48ec25f375868bf11622ed9ceb
alexRCallies <59710714+alexRCallies@users.noreply.github.com>
2020-02-18 10:36:50 -0600

    customer model

---
+++ TrashCollectionApp/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollectionApp.Models
+{
+    public class Customer
+    {
+        [key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public int Balance { get; set; }
+        public string PickUpDay { get; set; }
+
+    }
+}
