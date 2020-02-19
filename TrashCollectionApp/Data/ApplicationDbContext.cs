e527eb684b766633ea86ca1ca674d7d762550a09
alexRCallies <59710714+alexRCallies@users.noreply.github.com>
2020-02-19 07:59:10 -0600

    seeded employeee and customer to the database

--- TrashCollectionApp/Data/ApplicationDbContext.cs
+++ TrashCollectionApp/Data/ApplicationDbContext.cs
@@ -19,6 +19,10 @@ namespace TrashCollectionApp.Data
             base.OnModelCreating(builder);
             builder.Entity<IdentityRole>()
                 .HasData(new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" });
+            builder.Entity<IdentityRole>()
+                .HasData(new IdentityRole { Name = "Customer", NormalizedName = "CUSTOMER" });
+            builder.Entity<IdentityRole>()
+                .HasData(new IdentityRole { Name = "Employee", NormalizedName = "EMPLOYEE" });
         }
         public DbSet<Employee> employees { get; set; }
        public DbSet<Customer> customers { get; set; }
